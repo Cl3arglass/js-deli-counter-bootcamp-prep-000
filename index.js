511681aa7e84f224f3ced48678ba30fb74044196
Holden Altaffer <haltaffer@gmail.com>
2018-01-10 22:17:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// `var katzDeli = [];`
-//
-// 1. Build a function that a new customer will use when entering the deli. The function, `
-// takeANumber`, should accept the current line of people, `katzDeliLine`, along with the
-// new person's name as parameters. The function should return their position in line. And don't
-// go being too programmer-y and give them their index. These are normal people. If they are 7th in
-// line, tell them that. Don't get their hopes up by
-// telling them they are number 6 in line.
 
 function takeANumber(katzDeliLine, newPerson) {
-  // for (var i = 0; i < newPerson.length; i++) {
-  //   katzDeliLine.push(newPerson[i]);
-  // }
-  // return `Welcome, ${newPerson[i]}. You are number ${katzDeliLine.indexOf(newPerson[i]) + 1} in line.`
   katzDeliLine.push(newPerson);
   return `Welcome, ${newPerson}. You are number ${katzDeliLine.indexOf(newPerson) + 1} in line.`
+}
+
+// 2. Build a function `nowServing`. This function should return the first person
+// in line and then remove that individual from the line. If there is nobody in line,
+// it should return "There is nobody waiting to be served!"
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return katzDeliLine.shift()
+  }
 }
